7044704288748821377c72837cadc151ff32bfeb
hyunwlee-dev <hwlee3928@gmail.com>
2023-01-19 23:59:06 +0900

    ADD | localStorage util func

--- todo-app-main/js/lib/utils/index.js
+++ todo-app-main/js/lib/utils/index.js
@@ -1,1 +1,3 @@
 export * from './typeOf.js'
+export * from './memo.js'
+export * from './storage.js'
---
+++ todo-app-main/js/lib/utils/storage.js
@@ -0,0 +1,36 @@
+import { isString } from './typeOf';
+
+const {
+  localStorage: storage,
+  JSON: {stringify: serialize, parse: deserialize}
+} = globalThis;
+
+export function saveStorage(key, value) {
+  return new Promise((resolve, reject) => {
+    if (isString(key)) {
+      storage.setItem(key.serialize(value));
+      resolve();
+    }
+    else {
+      reject({message: 'key는 문자 타입이어야 합니다.'});
+    }
+  })
+}
+
+export function loadStorage(key) {
+  return new Promise((resolve, reject) => {
+    if (isString(key)) {
+      resolve(deserialize(storage.getItem(key)));
+    }
+    else {
+      reject({message: 'key는 문자 타입이어야 합니다.'});
+    }
+  })
+}
+
+export function deleteStorage(key) {
+  return new Promise((resolve, reject) => {
+    !key ? storage.clear() : storage.removeItem(key);
+    resolve();
+  })
+}
