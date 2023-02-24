b7409f1ee5ee489d94a5984d2a9343e54f5ae711
hayoenn2 <dwd1205@naver.com>
2023-02-25 02:42:46 +0900

    add | Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 여기서 name로 받으면 두개의 다른 onChange function을 만들 필요가 없다.
+  // 심플하게 폼을 컨트롤 가능하다.
+  const onChange = (e) => {
+    //console.log(e.target.name);
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        {/* value 준 이유 : input값을 가져다 쓰기 위해서 */}
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
