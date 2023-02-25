e05ffb560f44ab3290095d2a439754f6b25df7d7
hayoenn2 <dwd1205@naver.com>
2023-02-25 14:40:18 +0900

    add | Log Out 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,17 +2,23 @@ import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
           </>
         ) : (
-          <Route path="/" element={<Auth />} />
+          <>
+            <Route path="/" element={<Auth />}></Route>
+          </>
         )}
       </Routes>
     </Router>
